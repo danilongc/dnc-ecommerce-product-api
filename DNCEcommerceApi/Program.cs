84b39e1e8cef9602d822b6b29593476b1b09dd09
danilongc <danilonc.1986@gmail.com>
2023-05-08 02:05:18 -0300

    Added compose

--- DNCEcommerceApi/Program.cs
+++ DNCEcommerceApi/Program.cs
@@ -34,11 +34,14 @@ internal class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        // if (app.Environment.IsDevelopment())
+        // {
+        //     app.UseSwagger();
+        //     app.UseSwaggerUI();
+        // }
+
+         app.UseSwagger();
+         app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
